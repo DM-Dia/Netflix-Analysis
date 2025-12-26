29b09dc75e3edc6b4b8ac91b3523dc0dc1652bed
Devansi <128216642+DM-Dia@users.noreply.github.com>
2025-12-26 22:28:08 +0530

    updated columns

--- sql/schema.sql
+++ sql/schema.sql
@@ -3,13 +3,12 @@ CREATE TABLE netflix_titles (
     type VARCHAR(10),
     title VARCHAR(300),
     director VARCHAR(600),
-    cast VARCHAR(1200),
+    cast_member VARCHAR(1200),
     country VARCHAR(600),
-    date_added DATE,
+    date_added VARCHAR(15),
     release_year INT,
     rating VARCHAR(20),
-    duration_value INT,
-    duration_unit VARCHAR(20),
-    listed_in VARCHAR(300),
-    description VARCHAR(1000)
+    duration VARCHAR(30),
+    listed_in VARCHAR(500),
+    description VARCHAR(1500)
 );
